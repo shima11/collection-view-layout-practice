2e00db8abe8acbf72bdd3bec6025d443c9827bd6
Shima <shima.jin@icloud.com>
2021-02-04 13:13:58 +0900

    Initial Commit

---
+++ collection-view-layout-practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  collection-view-layout-practice
+//
+//  Created by jinsei_shima on 2021/02/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
